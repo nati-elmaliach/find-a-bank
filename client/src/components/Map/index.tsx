f8a0835c9e91f556825d59d6de7a4ff64e926128
nati-elmaliach <natielmaliach3197@gmail.com>
2022-04-16 23:19:52 +0300

    text

--- client/src/components/Map/index.tsx
+++ client/src/components/Map/index.tsx
@@ -16,7 +16,7 @@ const Map = () => {
       style={{ height: '80vh', width: '80wh', marginTop: '5rem' }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <Marker icon={getMapMarkerIcon('red')} position={userCoords}>
