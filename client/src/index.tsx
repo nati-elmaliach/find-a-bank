87458b43d7b1df58aecfe9bfd43322c4d6647f95
nati-elmaliach <natielmaliach3197@gmail.com>
2022-04-17 17:31:18 +0300

    Adding redux-persist to persist state between refresh

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
 import Home from './pages/Home';
 import Banks from './pages/Banks';
 
 import './index.css';
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
+let persistor = persistStore(store);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='banks-near-me' element={<Banks />} />
-      </Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='banks-near-me' element={<Banks />} />
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,22 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import appReducer from './appSlice';
 
+const reducers = combineReducers({
+  app: appReducer,
+});
+
+const persistConfig = {
+  key: 'app',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    app: appReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
